2e5f3402a546c8443f6e4a9dfe3a5808e13d5e7d
Adrian Perez <perezA@appacademy.us>
2015-02-05 14:04:09 -0800

    efc

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,5 +1,5 @@
 <?php
-    require_once(__DIR__ . "/../model/database.php");
+require_once(__DIR__ . "/../model/database.php");
 
     $connection = new mysqli($host, $username, $password);
     
@@ -17,20 +17,23 @@
       
       if($query){
           echo "Successfully created database: = . $database";
-      }
+        }
     }
     else {
         echo "Database already exists.";
     }
    
     $query = $connection->query("CREATE TABLE posts ("
-            . "id int(11)NOT MULL AUTO_INCREMENT,"
-            . "title varchat(255) NOT MULL"
-            . "post text NOT MULL"
+            . "id int(11)NOT NULL AUTO_INCREMENT,"
+            . "title varchar(255) NOT NULL,"
+            . "post text NOT NULL,"
             . "PRIMARY KEY (id))");
     
     if($query) {
-        echo "Succesfully create table: posts";
+        echo "Succesfully created table: posts";
+    }
+    else {
+        echo "$connection->error";
     }
     
     $connection->close();
