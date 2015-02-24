71ea5e22b1b732dac9e6d7849a4b0b6c1aeebd07
Adrian Perez <perezA@appacademy.us>
2015-02-23 21:57:50 -0800

    hd

--- view/register-form.php
+++ view/register-form.php
@@ -1,6 +1,10 @@
+<?php
+     require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1>Register</h1>
 
-<form method="post">
+<form method="post" action="<?php echo $path . "controller/create-post.php" ?>">">
     <div> 
         <label for="email">Email:</label> 
         <input type="text" name="email" />
