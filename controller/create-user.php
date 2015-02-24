37cc3783643a572d159621d685e7947752f97ddb
Adrian Perez <perezA@appacademy.us>
2015-02-24 14:02:28 -0800

    wef

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,2 +1,18 @@
 <?php
-echo "Successfully created user";
+    require_once (__DIR__ . "/../model/config.php");
+
+    $email = filter_input(INPUT_POST, "emial", FILTER_SANITIZE_EMAIL);
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_EMAIL);
+    $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_EMAIL);
+    
+    echo $password;
+    
+    $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
+    
+    echo $salt;
+    
+    
+    
+    
+    
+    
