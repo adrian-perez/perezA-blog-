ec744cc0a741e830fa0f582febc2db8e1192ca56
Adrian Perez <perezA@appacademy.us>
2015-02-23 19:26:36 -0800

    yf

--- view/register-form.php
+++ view/register-form.php
@@ -1,6 +1,23 @@
-<form>
+<h1>Register</h1>
+
+<form method="post">
+    <div> 
+        <label for="email">Email:</label> 
+        <input type="text" name="email" />
+    </div>
+    
     <div>
-        
+        <label for="username">Username: </label> 
+        <input type="text" name="username" />
+    </div>
+    
+    <div>
+        <label for="password">Password: </label> 
+        <input type="password" name="password" />
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
     </div>
 </form>
 
