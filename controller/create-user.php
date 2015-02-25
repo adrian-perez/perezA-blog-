0cc558dffbe5b5d2e06d5a99ebe1ea0909943c79
Adrian Perez <perezA@appacademy.us>
2015-02-25 14:01:07 -0800

    hg

--- controller/create-user.php
+++ controller/create-user.php
@@ -5,14 +5,21 @@
     $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_EMAIL);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_EMAIL);
     
-    echo $password;
-    
     $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$";
     
-    echo $salt;
+    $hashedPassword = crypt($password, $salt);
+
     
+    $query = $_SESSION["connection"]->query("INSERT INTO users SET "
+            . "email = '$email',"
+            . "username = '$username',"
+            . "password = '$hashedPassword',"
+            . "salt = '$salt'");
     
+    if($query){
+        echo "Successfully created user: $username";
+    }
     
-    
-    
-    
+    else {
+        echo "<p>" . $_SESSION["connection"]->error . "</p>";
+    }
